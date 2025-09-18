c70ee4b99c792dc5662adf7365cf376f03d21390
Metehan <metehancekci9@gmail.com>
2025-09-19 00:45:34 +0300

    Save Load system

---
+++ Assets/Scripts/AbilityTypeList.cs
@@ -0,0 +1,9 @@
+
+    public enum AbilityTypeList
+    {
+        Dash,       // Dash yeteneği
+        Jump,       // Jump (Zıplama) yeteneği
+        DoubleJump, // DoubleJump (Çift Zıplama) yeteneği
+        Heal,       // Heal (İyileşme) yeteneği
+                    // Diğer yetenek türleri buraya eklenebilir
+    }
