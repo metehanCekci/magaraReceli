e8c82a80bb3462757820c6929983caa945d36c1d
Metehan <metehancekci9@gmail.com>
2025-09-20 00:08:47 +0300

    Add save point system with position saving
    Introduced SavePoint MonoBehaviour and updated SaveSystem to save and load the player's position along with health and abilities. Added SavePoint components and BoxCollider2D triggers to scene objects for save spots. Updated SaveTest to use the new Save method signature. Also resolved merge conflicts in ThynnJump.aseprite.meta and adjusted sprite meta files for tilemaps.

--- Assets/Scripts/SavePoint.cs
+++ Assets/Scripts/SavePoint.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class SavePoint : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public SaveSystem saveSystem;
+
+    void Awake()
     {
-        
+        if (saveSystem == null)
+            saveSystem = FindObjectOfType<SaveSystem>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private float saveCooldown = 2f; // 2 saniye arayla kayıt yapılabilir
+    private float lastSaveTime = -10f;
+
+    void OnTriggerStay2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            if (Time.time > lastSaveTime + saveCooldown)
+            {
+                saveSystem.Save(other.gameObject, transform.position);
+                lastSaveTime = Time.time;
+                Debug.Log("Game saved at save point! (Bonfire position: " + transform.position + ")");
+            }
+        }
     }
 }
