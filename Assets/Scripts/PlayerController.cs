610307708697ff9e5db12741955f8d526da0a8d6
Metehan <metehancekci9@gmail.com>
2025-09-19 15:14:11 +0300

    aev

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -72,20 +72,20 @@ public class PlayerController : MonoBehaviour
 
     void OnEnable()
     {
-        if (Move)  Move.action.Enable();
+        if (Move) Move.action.Enable();
         if (Jump) { Jump.action.Enable(); Jump.action.performed += OnJumpPerformed; Jump.action.canceled += OnJumpCanceled; }
         if (Dash) { Dash.action.Enable(); Dash.action.performed += OnDashPerformed; }
-        if (Attack){ Attack.action.Enable(); Attack.action.performed += OnAttackPerformed; } // sadece performed
-        if (Heal)  { Heal.action.Enable(); Heal.action.performed += OnHealPerformed; }
+        if (Attack) { Attack.action.Enable(); Attack.action.performed += OnAttackPerformed; } // sadece performed
+        if (Heal) { Heal.action.Enable(); Heal.action.performed += OnHealPerformed; }
     }
 
     void OnDisable()
     {
-        if (Move)  Move.action.Disable();
+        if (Move) Move.action.Disable();
         if (Jump) { Jump.action.performed -= OnJumpPerformed; Jump.action.canceled -= OnJumpCanceled; Jump.action.Disable(); }
         if (Dash) { Dash.action.performed -= OnDashPerformed; Dash.action.Disable(); }
-        if (Attack){ Attack.action.performed -= OnAttackPerformed; Attack.action.Disable(); }
-        if (Heal)  { Heal.action.performed -= OnHealPerformed; Heal.action.Disable(); }
+        if (Attack) { Attack.action.performed -= OnAttackPerformed; Attack.action.Disable(); }
+        if (Heal) { Heal.action.performed -= OnHealPerformed; Heal.action.Disable(); }
     }
 
     void RecalcMaxJumps()
@@ -95,76 +95,76 @@ public class PlayerController : MonoBehaviour
     }
 
     void Update()
-{
-    float x = 0f;
-
-    if (Move != null && Move.action != null)
     {
-        // Move action'ını kullanarak hareket input'unu alıyoruz
-        if (Move.action.expectedControlType == "Vector2")
+        float x = 0f;
+
+        if (Move != null && Move.action != null)
         {
-            x = Move.action.ReadValue<Vector2>().x;
+            // Move action'ını kullanarak hareket input'unu alıyoruz
+            if (Move.action.expectedControlType == "Vector2")
+            {
+                x = Move.action.ReadValue<Vector2>().x;
+            }
+            else
+            {
+                x = Move.action.ReadValue<float>();
+            }
+
+            // Yürüyüş animasyonunu kontrol ediyoruz
+            if (Mathf.Abs(x) > 0.01f)
+            {
+                animator.SetBool("Walking", true); // Hareket ediyoruz, yürüyüş animasyonunu oynat
+            }
+            else
+            {
+                animator.SetBool("Walking", false); // Hareket etmiyoruz, yürüyüş animasyonu durur
+            }
+        }
+
+        moveInput = new Vector2(Mathf.Clamp(x, -1f, 1f), 0f);
+
+        // Anlık hareket
+        if (!isDashing)
+        {
+            rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+        }
+
+        // Yön değiştirme
+        if (Mathf.Abs(moveInput.x) > 0.01f)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(moveInput.x), 1, 1); // Yönü değiştiriyoruz
+        }
+
+        // Jump buffer consume
+        if (bufferCounter > 0f && (coyoteCounter > 0f || jumpCount < maxJumps))
+        {
+            DoJump();
+            bufferCounter = 0f;
+        }
+
+        // Variable jump (daha uzun zıplama)
+        if (!jumpHeld && rb.velocity.y > 0f)
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * variableJumpMultiplier);
+
+        // Ground / Coyote Check
+        if (IsGrounded())
+        {
+            coyoteCounter = coyoteTime;
+            jumpCount = 0;
         }
         else
         {
-            x = Move.action.ReadValue<float>();
+            coyoteCounter -= Time.deltaTime;
         }
 
-        // Yürüyüş animasyonunu kontrol ediyoruz
-        if (Mathf.Abs(x) > 0.01f)
-        {
-            animator.SetBool("Walking", true); // Hareket ediyoruz, yürüyüş animasyonunu oynat
-        }
-        else
-        {
-            animator.SetBool("Walking", false); // Hareket etmiyoruz, yürüyüş animasyonu durur
-        }
+        // Animator state
+        animator.SetBool("IsGrounded", IsGrounded());
     }
-
-    moveInput = new Vector2(Mathf.Clamp(x, -1f, 1f), 0f);
-
-    // Anlık hareket
-    if (!isDashing)
-    {
-        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
-    }
-
-    // Yön değiştirme
-    if (Mathf.Abs(moveInput.x) > 0.01f)
-    {
-        transform.localScale = new Vector3(Mathf.Sign(moveInput.x), 1, 1); // Yönü değiştiriyoruz
-    }
-
-    // Jump buffer consume
-    if (bufferCounter > 0f && (coyoteCounter > 0f || jumpCount < maxJumps))
-    {
-        DoJump();
-        bufferCounter = 0f;
-    }
-
-    // Variable jump (daha uzun zıplama)
-    if (!jumpHeld && rb.linearVelocity.y > 0f)
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * variableJumpMultiplier);
-
-    // Ground / Coyote Check
-    if (IsGrounded())
-    {
-        coyoteCounter = coyoteTime;
-        jumpCount = 0;
-    }
-    else
-    {
-        coyoteCounter -= Time.deltaTime;
-    }
-
-    // Animator state
-    animator.SetBool("IsGrounded", IsGrounded());
-}
 
 
     // ===== Input Callbacks =====
-    void OnJumpPerformed(InputAction.CallbackContext ctx){ jumpHeld = true; bufferCounter = jumpBufferTime; }
-    void OnJumpCanceled (InputAction.CallbackContext ctx){ jumpHeld = false; }
+    void OnJumpPerformed(InputAction.CallbackContext ctx) { jumpHeld = true; bufferCounter = jumpBufferTime; }
+    void OnJumpCanceled(InputAction.CallbackContext ctx) { jumpHeld = false; }
 
     void OnDashPerformed(InputAction.CallbackContext ctx)
     {
@@ -216,30 +216,30 @@ public class PlayerController : MonoBehaviour
         // dash bitişini Update/Fixed'de değil, korutinle de kapatabilirsin; gerek yoksa böyle kalabilir.
         Invoke(nameof(_EndDash), dashDuration);
     }
-    void _EndDash(){ isDashing = false; }
+    void _EndDash() { isDashing = false; }
 
     System.Collections.IEnumerator AttackSwing()
-{
-    isAttacking = true;
+    {
+        isAttacking = true;
 
-    // İlk animasyonu tetikle
-    animator?.SetTrigger("Swing1"); // İlk animasyonu tetikleyen trigger
+        // İlk animasyonu tetikle
+        animator?.SetTrigger("Swing1"); // İlk animasyonu tetikleyen trigger
 
-    if (attackHitbox) 
-        attackHitbox.gameObject.SetActive(true);
+        if (attackHitbox)
+            attackHitbox.gameObject.SetActive(true);
 
-    PlayOne(attackSound);
+        PlayOne(attackSound);
 
-    yield return new WaitForSeconds(attackDuration);
+        yield return new WaitForSeconds(attackDuration);
 
-    // İlk animasyon bittiğinde, ikinci animasyonu tetikle
-    animator?.SetTrigger("Swing2"); // İkinci animasyonu tetikleyen trigger
+        // İlk animasyon bittiğinde, ikinci animasyonu tetikle
+        animator?.SetTrigger("Swing2"); // İkinci animasyonu tetikleyen trigger
 
-    if (attackHitbox) 
-        attackHitbox.gameObject.SetActive(false);
-    
-    isAttacking = false;
-}
+        if (attackHitbox)
+            attackHitbox.gameObject.SetActive(false);
+
+        isAttacking = false;
+    }
 
     System.Collections.IEnumerator HealRoutine()
     {
@@ -251,7 +251,7 @@ public class PlayerController : MonoBehaviour
         isHealing = false;
     }
 
-    void PlayOne(AudioClip clip){ if (clip && audioSource) audioSource.PlayOneShot(clip); }
+    void PlayOne(AudioClip clip) { if (clip && audioSource) audioSource.PlayOneShot(clip); }
 
     void OnDrawGizmosSelected()
     {
